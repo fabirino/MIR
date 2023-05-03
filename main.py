98dff453560ca21fc72118747d324600cad7355d
FabioS <fabalexandre8@gmail.com>
2023-05-03 16:49:05 +0100

    falta um pormenor do exercicio 4

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def librosa_stats(y, sr):
 
     # Temporais
     f0 = librosa.yin(y, fmin=20, fmax=11025)
-    f0[f0==11025] = 0
+    f0[f0 == 11025] = 0
     rms = librosa.feature.rms(y=y).flatten()
     zcr = librosa.feature.zero_crossing_rate(y=y).flatten()
 
@@ -108,8 +108,8 @@ def all_features_array():
             extract_features, 1, librosa.feature.spectral_rolloff(y=y)).flatten()
 
         # Temporais
-        f0_aux = librosa.yin(y, fmin =20, fmax = 11025)
-        f0_aux[f0_aux==11025] = 0
+        f0_aux = librosa.yin(y, fmin=20, fmax=11025)
+        f0_aux[f0_aux == 11025] = 0
         f0 = np.apply_along_axis(
             extract_features, 0, f0_aux)
         rms = np.apply_along_axis(
@@ -121,7 +121,7 @@ def all_features_array():
 
         aux = np.concatenate((mfcc, centroid, bswth, contrast, flatness, rollof,
                               f0, rms, zcr, bpm)).astype(float)
-    
+
         # print(aux)
         array[index] = aux
         index += 1
@@ -145,7 +145,8 @@ def Exercicio2():
     # print(normalized)
 
     # 2.1.3
-    np.savetxt("./Features/top100_feat_normalized.csv", normalized,delimiter=";", fmt='%f')
+    np.savetxt("./Features/top100_feat_normalized.csv",
+               normalized, delimiter=";", fmt='%f')
 
     # 2.2.1
     music_file = "./MER_audio_dataset/audios/MT0000004637.mp3"
@@ -158,7 +159,7 @@ def Exercicio2():
     # 2.2.2
     all_features = all_features_array()  # para todas as features
 
-    #save features
+    # save features
     np.savetxt("./MER_audio_dataset/not_Norm_features.csv",
                all_features, delimiter=",", fmt='%f')
 
@@ -168,87 +169,201 @@ def Exercicio2():
     # 2.2.4
     np.savetxt("./MER_audio_dataset/normalized_features.csv",
                all_features_normalized, delimiter=";", fmt='%f')
-    
-#==================================================================================================
+
+# ==================================================================================================
+
+
 def euclidean_distance(a, b):
     return np.sqrt(np.sum(np.square(a - b)))
 
+
 def manhattan_distance(a, b):
     return np.sum(np.abs(a - b))
+
 
 def cosine_distance(a, b):
     return 1 - np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
-def E3_2(matrix,name):
+def E3_2(matrix, name):
 
-    similarity_matrix = np.zeros((900,900))
+    similarity_matrix = np.zeros((900, 900))
     for i in range(900):
         for j in range(i+1, 900):
             d = euclidean_distance(matrix[i, :], matrix[j, :])
             similarity_matrix[i, j] = d
             similarity_matrix[j, i] = d
-    np.savetxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_euclidean.csv", similarity_matrix, delimiter=";", fmt='%f')
-    
-    similarity_matrix = np.zeros((900,900))
+    np.savetxt(
+        f"./MER_audio_dataset/SimilarityMatrix/{name}_euclidean.csv", similarity_matrix, delimiter=";", fmt='%f')
+
+    similarity_matrix = np.zeros((900, 900))
     for i in range(900):
         for j in range(i+1, 900):
             d = manhattan_distance(matrix[i, :], matrix[j, :])
             similarity_matrix[i, j] = d
             similarity_matrix[j, i] = d
-    np.savetxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_manhattan.csv", similarity_matrix, delimiter=";", fmt='%f')
+    np.savetxt(
+        f"./MER_audio_dataset/SimilarityMatrix/{name}_manhattan.csv", similarity_matrix, delimiter=";", fmt='%f')
 
-    similarity_matrix = np.zeros((900,900))
+    similarity_matrix = np.zeros((900, 900))
     for i in range(900):
         for j in range(i+1, 900):
             d = cosine_distance(matrix[i, :], matrix[j, :])
             similarity_matrix[i, j] = d
             similarity_matrix[j, i] = d
-    np.savetxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_cosine.csv", similarity_matrix, delimiter=";", fmt='%f')
+    np.savetxt(
+        f"./MER_audio_dataset/SimilarityMatrix/{name}_cosine.csv", similarity_matrix, delimiter=";", fmt='%f')
+
 
 def get_song_index(song_name):
     songs = os.listdir("./MER_audio_dataset/audios")
     songs.sort()
-    return np.where(np.array(songs)==song_name)[0][0]
+    return np.where(np.array(songs) == song_name)[0][0]
+
 
 def get_song_name(song_index):
     songs = os.listdir("./MER_audio_dataset/audios")
     songs.sort()
     return songs[song_index]
 
+
 def rank_songs(name):
     for song in os.listdir("./MER_audio_dataset/audios"):
         i = get_song_index(song)
-        song_row = np.loadtxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_euclidean.csv", delimiter=";")[i]
-        sorted_indexes = np.argsort(song_row)[1:21]
-        top_songs_names = list(map(lambda song_index: get_song_name(song_index), sorted_indexes))
+        song_row = np.loadtxt(
+            f"./MER_audio_dataset/SimilarityMatrix/{name}_euclidean.csv", delimiter=";")[i]
+        sorted_indexes = np.argsort(song_row)[0:21]
+        top_songs_names = list(
+            map(lambda song_index: get_song_name(song_index), sorted_indexes))
         os.makedirs(f"./MER_audio_dataset/Rankings/{song}", exist_ok=True)
-        np.savetxt(f"./MER_audio_dataset/Rankings/{song}/{name}_euclidean.csv", top_songs_names, fmt="%s")
+        np.savetxt(
+            f"./MER_audio_dataset/Rankings/{song}/{name}_euclidean.csv", top_songs_names, fmt="%s")
 
-        song_row = np.loadtxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_manhattan.csv", delimiter=";")[i]
-        sorted_indexes = np.argsort(song_row)[1:21]
-        top_songs_names = list(map(lambda song_index: get_song_name(song_index), sorted_indexes))
+        song_row = np.loadtxt(
+            f"./MER_audio_dataset/SimilarityMatrix/{name}_manhattan.csv", delimiter=";")[i]
+        sorted_indexes = np.argsort(song_row)[0:21]
+        top_songs_names = list(
+            map(lambda song_index: get_song_name(song_index), sorted_indexes))
         os.makedirs(f"./MER_audio_dataset/Rankings/{song}", exist_ok=True)
-        np.savetxt(f"./MER_audio_dataset/Rankings/{song}/{name}_manhattan.csv", top_songs_names, fmt="%s")
-        
-        song_row = np.loadtxt(f"./MER_audio_dataset/SimilarityMatrix/{name}_cosine.csv", delimiter=";")[i]
-        sorted_indexes = np.argsort(song_row)[1:21]
-        top_songs_names = list(map(lambda song_index: get_song_name(song_index), sorted_indexes))
+        np.savetxt(
+            f"./MER_audio_dataset/Rankings/{song}/{name}_manhattan.csv", top_songs_names, fmt="%s")
+
+        song_row = np.loadtxt(
+            f"./MER_audio_dataset/SimilarityMatrix/{name}_cosine.csv", delimiter=";")[i]
+        sorted_indexes = np.argsort(song_row)[0:21]
+        top_songs_names = list(
+            map(lambda song_index: get_song_name(song_index), sorted_indexes))
         os.makedirs(f"./MER_audio_dataset/Rankings/{song}", exist_ok=True)
-        np.savetxt(f"./MER_audio_dataset/Rankings/{song}/{name}_cosine.csv", top_songs_names, fmt="%s")
+        np.savetxt(
+            f"./MER_audio_dataset/Rankings/{song}/{name}_cosine.csv", top_songs_names, fmt="%s")
+
 
 def Exercicio3():
-    all_features_normalized = np.loadtxt("./MER_audio_dataset/normalized_features.csv", delimiter=";")
+    all_features_normalized = np.loadtxt(
+        "./MER_audio_dataset/normalized_features.csv", delimiter=";")
     top100 = np.loadtxt("./Features/top100_feat_normalized.csv", delimiter=";")
     os.makedirs(f"./MER_audio_dataset/SimilarityMatrix", exist_ok=True)
 
-    E3_2(top100,"top100")
-    E3_2(all_features_normalized,"librosa")
+    E3_2(top100, "top100")
+    E3_2(all_features_normalized, "librosa")
 
     os.makedirs(f"./MER_audio_dataset/Rankings", exist_ok=True)
     rank_songs("librosa")
     rank_songs("top100")
 
+# ==================================================================================================
+
+
+def create_sim_table(metadados):
+    table = np.zeros((len(metadados), len(metadados)))
+    for i in range(len(metadados)):
+        mood1 = [i.strip("\" ") for i in metadados[i][3].split(";")]
+        gen1 = [i.strip("\" ") for i in metadados[i][4].split(";")]
+
+        for j in range(i, len(metadados)):
+            count = 0
+
+            # Artist
+            if metadados[i][1] == metadados[j][1]:
+                count += 1
+
+            # Quadrant
+            if metadados[i][2] == metadados[j][2]:
+                count += 1
+
+            # MoodsStrSplit
+            mood2 = [i.strip("\" ") for i in metadados[j][3].split(";")]
+            matches = np.intersect1d(mood1, mood2)
+            count += len(matches)
+
+            # GenreStr
+            gen2 = [i.strip("\" ") for i in metadados[j][4].split(";")]
+            matches2 = np.intersect1d(gen1, gen2)
+            count += len(matches2)
+
+            table[i][j] = count
+            table[j][i] = count
+
+    return table
+
+
+def top20_musics(metadados, sim_table):
+    for i in range(len(metadados)):
+        row = sim_table[i]
+        sorted_row = np.argsort(row)[::-1]
+        top20 = sorted_row[:21]
+        top20_musics = metadados[top20][:, 0]
+        top20_musics = np.char.add(top20_musics, ".mp3")
+
+        song = metadados[i][0].strip("\" ")
+        score = row[top20]
+
+        lib_euclidean = np.loadtxt(
+            f"./MER_audio_dataset/Rankings/{song}.mp3/librosa_euclidean.csv", dtype=str)
+        lib_euclidean = np.char.strip(lib_euclidean, "\" ")
+
+        lib_cosine = np.loadtxt(
+            f"./MER_audio_dataset/Rankings/{song}.mp3/librosa_cosine.csv", dtype=str)
+        lib_cosine = np.char.strip(lib_cosine, "\" ")
+
+        lib_manhattan = np.loadtxt(
+            f"./MER_audio_dataset/Rankings/{song}.mp3/librosa_manhattan.csv", dtype=str)
+        lib_manhattan = np.char.strip(lib_manhattan, "\" ")
+
+        der = len(np.intersect1d(lib_euclidean, top20_musics)) - 1
+        dmr = len(np.intersect1d(lib_manhattan, top20_musics)) - 1
+        dcr = len(np.intersect1d(lib_cosine, top20_musics)) - 1
+
+        mean1 = 0
+        mean2 = 0
+        mean3 = 0
+
+        with open(f"./MER_audio_dataset/Rankings/{song}.mp3/ranking.txt", "w") as f:
+            f.write(f"Query = '{song}.mp3'\n\n")
+            f.write(f"Ranking = Ranking: FMrosa, Euclidean-------------\n{lib_euclidean}\n\n")
+            f.write(f"Ranking = Ranking: Metadata-------------\n{top20_musics}\n\n")
+            f.write(f"Score Metadata = {score}\n\n\n")
+            f.write(f"Precision der: {der} *** {mean1}\n")
+            f.write(f"Precision dmr: {dmr} *** {mean2}\n")
+            f.write(f"Precision dcr: {dcr} *** {mean3}\n")
+
+
+def Exercicio4():
+    metadados = np.loadtxt(
+        "./MER_audio_dataset/panda_dataset_taffc_metadata.csv", dtype=str, delimiter=",")
+    metadados = metadados[:, [0, 1, 3, 9, 11]]
+    metadados = metadados[1:, :]
+    metadados = np.char.strip(metadados, "\" ")
+
+    # Achei mais facil fazer o segundo exercicio primeiro que assim ja ficamos com a tabela
+    # 4.1.2
+    # sim_table = create_sim_table(metadados)
+    sim_table = np.loadtxt(
+        "./MER_audio_dataset/SimilarityMatrix/sim_table.csv", delimiter=";")
+    # np.savetxt("./MER_audio_dataset/sim_table.csv", sim_table, delimiter=";", fmt='%d')
+    # 4.1.1
+    top20_musics(metadados, sim_table)
+
 
 if __name__ == "__main__":
-    Exercicio3()
+    Exercicio4()
